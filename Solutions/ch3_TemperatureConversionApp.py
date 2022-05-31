b223d89f75719b2a92a201498f1bc4e1e82483c5
Casey-Lux <casey.lux.000@gmail.com>
2022-05-31 11:07:44 -0500

    Exercise 3 fixed

--- Solutions/ch3_TemperatureConversionApp.py
+++ Solutions/ch3_TemperatureConversionApp.py
@@ -3,11 +3,11 @@ print("Welcome to Temperature conversion App!")
 print("This app convert temperature in celsius degrees to fahrenheit and kelvin.")
 
 # Data input
-print("\nInput temperature in Celsius degrees pleace:")
-celsius = float(input(">> "))
+print("\nInput temperature in fahrenheit degrees pleace:")
+fahrenheit = float(input(">> "))
 
 # Process
-fahrenheit = float((celsius * 9/5) + 32)
+celsius = float((fahrenheit - 32) * 5/9)
 kelvin = float(celsius + 273.15)
 
 fahrenheit = round(fahrenheit, 4)
@@ -15,8 +15,8 @@ kelvin = round(kelvin, 4)
 celsius = round(celsius, 4)
 
 # Data output
-print("celsius  |   fahrenheit  |   kelvin")
+print("fahrenheit  |   celsius  |   kelvin")
 print("-----------------------------------")
-print(celsius, "    |   ", fahrenheit, "    |   ", kelvin)
+print(fahrenheit, "    |   ", celsius, "    |   ", kelvin)
 
 print("\n Thanks for use this program")
